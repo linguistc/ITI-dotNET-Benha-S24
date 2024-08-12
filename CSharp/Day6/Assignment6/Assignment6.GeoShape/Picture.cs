25ff8586a2e126f66eb068a7cebf8d796047f6d8
ahmedsameh <ahmadsameh1@proton.me>
2024-08-12 13:59:19 +0300

    Moving Picture type to Picture.cs.

---
+++ CSharp/Day6/Assignment6/Assignment6.GeoShape/Picture.cs
@@ -0,0 +1,22 @@
+﻿namespace Assignment6.GeoShape
+{
+    public class Picture
+    {
+        public GeoShape[] shapes;
+        
+
+        public Picture(GeoShape[] shapes) => this.shapes = shapes;
+            
+        
+
+        public double Area()
+        {
+            double sum = 0;
+            foreach (var shape in shapes)
+            {
+                sum += shape.Area();
+            }
+            return sum;
+        }
+    }
+}
